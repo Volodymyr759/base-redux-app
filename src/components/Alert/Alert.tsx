c545bcccf4c22087b6fe01850779eb1b279fe88c
Volodia <volodymyr.m@eivolo.com>
2021-12-08 19:57:37 +0200

    added types for middleware

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -2,7 +2,7 @@ import { Props } from "./types";
 
 export function Alert(props: Props): React.ReactElement {
     return (
-        <div className="alert alert-warning" role="alert">
+        <div className="alert alert-warning" role="alert" style={{ marginTop: '30px' }}>
             {props.text}
         </div>
     )
--- src/store/middleware.tsx
+++ src/store/middleware.tsx
@@ -1,14 +1,20 @@
+import { Dispatch } from 'react';
+import { Action } from 'redux';
 import { showAlert } from './app/actions';
 import { CREATE_POST } from './posts/types';
+import { IPost } from './types';
 
 const forbidden = ['php', 'spam']
 
-// @ts-ignore
-export function forbiddenWordsMiddleware({ dispatch }) {
-    // @ts-ignore
-    return function (next) {
-        // @ts-ignore
-        return function (action) {
+interface IPostAction extends Action {
+    type: string;
+    payload: IPost;
+}
+
+export function forbiddenWordsMiddleware({ dispatch }: any) {
+    console.log('dispatch: ', dispatch)
+    return function (next: Dispatch<IPostAction>) {
+        return function (action: IPostAction) {
             if (action.type === CREATE_POST) {
                 const found = forbidden.filter(w => action.payload.title.includes(w))
                 if (found.length) {
